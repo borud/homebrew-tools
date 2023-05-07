d5288689e490bb0dd2f2b966454a6f733c2e4123
Bjorn Borud <borud@lab5e.com>
2023-05-07 18:37:49 +0200

    bumped tun version

--- tun.rb
+++ tun.rb
@@ -2,17 +2,17 @@
 class Tun <  Formula
 	desc "multi hop reverse proxy"
 	homepage "https://github.com/borud/tun"
-	version "0.1.2"
+	version "0.1.3"
 
 	on_macos do
 		if Hardware::CPU.intel?
-			url "https://github.com/borud/tun/releases/download/v0.1.2/tun.amd64-macos.zip"
-			sha256 "57872ad5a12310d4f7fe972c66921357fb653c12c60c44294026ea314e529b51"
+			url "https://github.com/borud/tun/releases/download/v0.1.3/tun.amd64-macos.zip"
+			sha256 "ebbdaa4e84b5a4933f1983e0f6e6f5b7cb34eaa9cc8749198b65f1c9f98c478a"
 		end
 
 		if Hardware::CPU.arm?
-			url "https://github.com/borud/tun/releases/download/v0.1.2/tun.arm64-macos.zip"
-			sha256 "e233a9b8da8308836a7495b135dc1586e520330865cb2b8fae9b94ea4461a0e9"
+			url "https://github.com/borud/tun/releases/download/v0.1.3/tun.arm64-macos.zip"
+			sha256 "18c0635461b55efc3c6b53f009d6f16dbb8755cf8bf440daa21ff86fd403afd2"
 		end
 	end
 
